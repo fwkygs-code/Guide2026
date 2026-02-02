e6519f2816e8b500607611d9e637d23a1c369fb4
Your New Name <your-new-email@example.com>
2026-02-02 06:25:50 +0200

    .

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,22 @@ import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
 
+alert("FRONTEND UPDATED");
+
+console.log('[BOOT] index.js executing');
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// BINARY SEARCH TEST: Does React mount at all?
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div style={{ color: 'red', fontSize: '40px', padding: '50px' }}>
+    REACT MOUNTS - If you see this, React is working
+  </div>
 );
+
+// TODO: If you see red text above, restore this code:
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
