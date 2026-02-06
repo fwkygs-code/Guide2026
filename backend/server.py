3cba34c99e234bb24f01ab7e1495a99742e7e2b2
Your New Name <your-new-email@example.com>
2026-02-06 05:13:57 +0200

    .

--- backend/server.py
+++ backend/server.py
@@ -14,6 +14,7 @@ import json
 import logging
 import os
 from fastapi import FastAPI, HTTPException, Depends, Request, BackgroundTasks, status, APIRouter, Query, File, UploadFile, Form, Header, Body, Cookie
+from fastapi.exceptions import RequestValidationError
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse, HTMLResponse, FileResponse, StreamingResponse, PlainTextResponse, RedirectResponse
